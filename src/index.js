d50abde2fb682e9b7c0c7630bba817283b785e4b
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-30 16:34:22 +0100

    Working on headers

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
 );
 
 const securityHeaders = {
-  'Content-Security-Policy': "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; connect-src 'self'; font-src 'self'; object-src 'none'; media-src 'self'",
+  'Content-Security-Policy': "default-src 'self'; script-src 'self'; style-src 'self'; img-src /img; connect-src 'self'; font-src 'self'; object-src 'none'; media-src 'self'",
   'X-Frame-Options': 'SAMEORIGIN',
   'X-Content-Type-Options': 'nosniff',
   'Referrer-Policy': 'strict-origin-when-cross-origin',
