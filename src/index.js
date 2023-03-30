d449c15cd2db3ae4a0893d0e466c3722a9ebaa78
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-30 16:15:37 +0100

    adding security headers

--- src/index.js
+++ src/index.js
@@ -13,6 +13,17 @@ root.render(
   </React.StrictMode>
 );
 
+const headers = {
+  'Content-Security-Policy': "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; connect-src 'self'; font-src 'self'; object-src 'none'; media-src 'self'",
+  'X-Frame-Options': 'SAMEORIGIN',
+  'X-Content-Type-Options': 'nosniff',
+  'Referrer-Policy': 'strict-origin-when-cross-origin',
+  'Permissions-Policy': 'camera=(), microphone=(), geolocation=()'
+};
+
+Object.entries(headers).forEach(([header, value]) => {
+  document.header(header, value);
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
